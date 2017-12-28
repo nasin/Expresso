03fbd06f714315acd6f57159d77669b6c4a654eb
Nancy Dillon <nanc.dillon@gmail.com>
2017-12-28 15:54:20 -0500

    capstone project files first commit

---
+++ api/employees.js
@@ -0,0 +1,116 @@
+const express = require('express');
+const employeesRouter = express.Router();
+
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+
+const timesheetsRouter = require('./timesheets.js');
+
+employeesRouter.use('/:employeeId/timesheets',timesheetsRouter);
+
+employeesRouter.param('employeeId', (req, res, next, employeeId) => {
+  const sql = 'SELECT * FROM Employee WHERE Employee.id = $employeeId';
+  const values = {$employeeId: employeeId};
+  db.get(sql, values, (error, employee) => {
+    if (error) {
+      next(error);
+    } else if (employee) {
+      req.employee = employee;
+      next();
+    } else {
+      res.sendStatus(404);
+    }
+  });
+});
+
+employeesRouter.get('/', (req, res, next) => {
+  db.all('SELECT * FROM Employee WHERE Employee.is_current_employee = 1',
+    (err, employees) => {
+      if (err) {
+        next(err);
+      } else {
+        res.status(200).json({employees: employees});
+      }
+    });
+});
+
+employeesRouter.get('/:employeeId', (req, res, next) => {
+  res.status(200).json({employee: req.employee});
+});
+
+employeesRouter.post('/', (req, res, next) => {
+  const name = req.body.employee.name;
+  const position = req.body.employee.position;
+  const wage = req.body.employee.wage;
+
+  if (!name || !position || !wage ) {
+    return res.sendStatus(400);
+  }
+
+  const sql = 'INSERT INTO Employee (name, position, wage, is_current_employee)' +
+      'VALUES ($name, $position, $wage, $isCurrentEmployee)';
+  const values = {
+    $name: name,
+    $position: position,
+    $wage: wage,
+    $isCurrentEmployee: 1
+  };
+
+  db.run(sql, values, function(error) {
+    if (error) {
+      next(error);
+    } else {
+      db.get(`SELECT * FROM Employee WHERE Employee.id = ${this.lastID}`,
+        (error, employee) => {
+          res.status(201).json({employee: employee});
+        });
+    }
+  });
+});
+
+employeesRouter.put('/:employeeId', (req, res, next) => {
+  const name = req.body.employee.name;
+  const position = req.body.employee.position;
+  const wage = req.body.employee.wage;
+    
+  if (!name || !position || !wage ) {
+    return res.sendStatus(400);
+  }
+
+  const sql = 'UPDATE Employee SET name = $name, position = $position, wage = $wage WHERE Employee.id = $employeeId';
+  const values = {
+    $name: name,
+    $position: position,
+    $wage: wage,
+   $employeeId: req.params.employeeId
+  };
+
+  db.run(sql, values, (error) => {
+    if (error) {
+      next(error);
+    } else {
+      db.get(`SELECT * FROM Employee WHERE Employee.id = ${req.params.employeeId}`,
+        (error, employee) => {
+          res.status(200).json({employee: employee});
+        });
+    }
+  });
+});
+
+employeesRouter.delete('/:employeeId', (req, res, next) => {
+  const sql = 'UPDATE Employee SET is_current_employee = 0 WHERE Employee.id = $employeeId';
+  const values = {$employeeId: req.params.employeeId};
+
+  db.run(sql, values, (error) => {
+    if (error) {
+      next(error);
+    } else {
+      db.get(`SELECT * FROM Employee WHERE Employee.id = ${req.params.employeeId}`,
+        (error, employee) => {
+          res.status(200).json({employee: employee});
+        });
+    }
+  });
+});
+
+module.exports = employeesRouter;
